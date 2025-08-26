6aa2379081a9186b5d59b56fb8b33fc68bd9a227
kumagai <sdt22csc0005@edu.sca.ac.jp>
2025-08-26 19:21:21 +0900

    Csvからマップを作る機能を作成

--- Helios/Assets/Scripts/CsvReader.cs
+++ Helios/Assets/Scripts/CsvReader.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Collections.Generic;
 public class CsvReader : MonoBehaviour
 {
-    private List<string[]> Read(TextAsset csvData) //引数に入力したCSVファイルをリストに変換する関数
+    protected List<string[]> Read(TextAsset csvData) //引数に入力したCSVファイルをリストに変換する関数
     {
         List<string[]> datas = new List<string[]>();
         StringReader reader = new StringReader(csvData.text);
