450e5bc1e88df44e8d12a922f3c94fd1217b7dec
hatake <sdt22csc0012@edu.sca.ac.kp>
2025-08-30 19:49:02 +0900

    タイマー機能の基盤

---
+++ Helios/Assets/Scripts/MainGame/TimeLine.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class TimeLine : MonoBehaviour
+{
+    protected float timeLine;
+    private float[] times=new float[3];
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        times = new float[3]
+        {
+            210.0f,210.0f,180.0f
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLine += Time.deltaTime;
+    }
+
+    protected void timerReset()
+    {
+        timeLine = 0.0f;
+
+    }
+}
