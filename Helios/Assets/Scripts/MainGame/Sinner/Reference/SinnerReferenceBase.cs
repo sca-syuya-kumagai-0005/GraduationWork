069c5abd1c587852c80b350c914944f99279d17b
kumagai <sdt22csc0005@edu.sca.ac.jp>
2025-11-29 18:49:25 +0900

    日数に応じて区画を生成

--- Helios/Assets/Scripts/MainGame/Sinner/Reference/SinnerReferenceBase.cs
+++ Helios/Assets/Scripts/MainGame/Sinner/Reference/SinnerReferenceBase.cs
@@ -5,22 +5,56 @@ public class SinnerReferenceBase : MonoBehaviour
      
     public struct SinnerRisk
     {
-        public const string RISK_LUMENIS = "Lumenis";
-        public const string RISK_VELGRA = "Velgra";
-        public const string RISK_DRAVEX = "Dravex";
-        public const string RISK_ZERATH = "Zerath";
-        public const string RISK_OBLIVARA = "Oblivara";
+        public const string LUMENIS = "Lumenis";
+        public const string VELGRA = "Velgra";
+        public const string DRAVEX = "Dravex";
+        public const string ZERATH = "Zerath";
+        public const string OBLIVARA = "Oblivara";
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public struct SinnerType
     {
-        
+        public const string SECRA = "Secra";
+        public const string VIGIL = "Vigil";
+        public const string HAZRA = "Hazra";
+        public const string CATRA = "Catra";
+        public const string NULLA = "Nulla";
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Serializable]
+    public struct SinnerInfomation
     {
-        
+        public string name;
+        public int id;
+        public string risk;
+        public string type;
+        public string abnormal;
+        public string[] condition;
+        public string apperance;
+        public string exeplanation;
+        public string[] overView;
+    }
+
+
+    public struct SortFlags
+    {
+        public struct Risk
+        {
+           public bool lumenis;
+           public bool velgra;
+           public bool dravex;
+           public bool zerath;
+           public bool oblivara;
+        }
+
+        public struct Type
+        {
+            public bool secra;
+            public bool vigil;
+            public bool hazra;
+            public bool catra;
+            public bool nulla;
+        }
+       
     }
 }
