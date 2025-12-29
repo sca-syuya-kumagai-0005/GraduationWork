63a2c845756d2121fb37f63400dd43bfb9dc6d89
AlUEO <sdt22csc0005@edu.sca.ac.jp>
2025-12-30 03:35:36 +0900

    配達ルート選択を多少分かりやすくした。多少

---
+++ Helios/Assets/Scripts/MainGame/Map/Guide.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Guide : MonoBehaviour
+{
+    [SerializeField] private GameObject[] driverGuideMarks;
+    [SerializeField] private GameObject penGuideMark;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
