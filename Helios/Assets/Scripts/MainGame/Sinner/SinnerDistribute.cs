856816a0c7ea869daa3b9a54b47141f23e44dbcb
hatake <sdt22csc0012>
2026-02-05 19:09:12 +0900

    プール変更

--- Helios/Assets/Scripts/MainGame/Sinner/SinnerDistribute.cs
+++ Helios/Assets/Scripts/MainGame/Sinner/SinnerDistribute.cs
@@ -8,7 +8,7 @@ public class SinnerDistribute : MonoBehaviour
 {
     [SerializeField]
     private GameObject sinnerObject;
-    private const int poolSize = 3;
+    private const int poolSize = 4;
     private List<Object> sinnerComponents = new List<Object>()
     {
         new ItemID_001(),
@@ -34,14 +34,19 @@ public class SinnerDistribute : MonoBehaviour
         new ItemID_021(),
         new ItemID_022(),
         new ItemID_023(),
+        new ItemID_024(),
+        new ItemID_025(),
+        new ItemID_026(),
+        new ItemID_031()
     };
     private List<GameObject>[] sinnerHousedObjects = new List<GameObject>[10];
     public List<GameObject>[] GetSinnerHousedObjects { get { return sinnerHousedObjects; } }
     private List<int>[] sinnerPools = new List<int>[poolSize]
     {
-        new List<int> { 2,4, 5, 6, 7, 8, 9,12,13,23},
-        new List<int> { 1,3,16,10,11,14,19,20,17,18},
-        new List<int> { 21},
+        new List<int> { 2, 4, 5,12,13,16,17,23,12,13},
+        new List<int> { 3 ,6, 7,19,20,21,22,24,27,20},
+        new List<int> { 1, 8, 9,10,14,25,26, 9,10,14},
+        new List<int> {31}
         //new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
         //new List<int> { 11,12,13,14,15,16,17,18,19,20 },
         //new List<int> { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 },
@@ -53,12 +58,14 @@ public class SinnerDistribute : MonoBehaviour
         new List<GameObject>(),
         new List<GameObject>(),
         new List<GameObject>(),
+        new List<GameObject>()
     };
     private List<int> totalSinnerPool = new List<int>();
     private bool[] housed = new bool[maxSinners];
     private int standbySinners;
     private SaveDataManager saveDataManager;
     private TutorialMG tutorialMG;
+    private GameObject unmaskBlock;
 
     //各区画に住むシナーを入れるリスト
     private List<int>[] plotContainedSinner = new List<int>[10];
@@ -72,10 +79,11 @@ public class SinnerDistribute : MonoBehaviour
 
     private void Start()
     {
+        unmaskBlock = GameObject.Find("UnMaskBlock").gameObject;
         tutorialMG = GameObject.Find("TutorialMG").GetComponent<TutorialMG>();
+        //unmaskBlock.SetActive(tutorialMG.IsTutorial);
+        //tutorialMG.gameObject.SetActive(tutorialMG.IsTutorial);
         saveDataManager = GameObject.Find("SaveManager").GetComponent<SaveDataManager>();　
-       
-       
        
         for (int i = 0; i < plotContainedSinner.Length; i++)
         {
@@ -95,9 +103,8 @@ public class SinnerDistribute : MonoBehaviour
             int gamePhase = GetGamePhase(saveDataManager.Days);
             Distribute(gamePhase);
             saveDataManager.HousedSinner = housed;
-        }
 
-       
+        }
     }
 
     private void Distribute(int gamePhase)
