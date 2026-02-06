0c28e23a6f6b9966e480b1b5a1d03d907450005a
hatake <sdt22csc0012>
2026-02-06 13:41:43 +0900

    023修正

--- Helios/Assets/Scripts/MainGame/Sinner/MouseNoise.cs
+++ Helios/Assets/Scripts/MainGame/Sinner/MouseNoise.cs
@@ -14,6 +14,7 @@ public class MouseNoise : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        radius = 10;
         isNoise = false;
         StartCoroutine(DrunkCorsor());
     }
